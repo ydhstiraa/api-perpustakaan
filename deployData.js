e72a82f4525a2f630e1c6818ec5399925d878cbf
ydhstiraa <iilham512@gmail.com>
2023-05-23 16:10:44 +0700

    First Deploy API Perpustakaan

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Yudhistira Ilham'; // TODO: replace this
+
+const studentId = 'FE4765948'; // TODO: replace this
+
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
